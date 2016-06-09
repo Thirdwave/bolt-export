ae4ca33aebcee3c551a01313b56d9efa2868ea7d
Guido Gautier <ggautier@thirdwave.nl>
2016-06-09 16:22:35 +0200

    First beta version

--- src/Controller.php
+++ src/Controller.php
@@ -1,8 +1,14 @@
 <?php namespace Bolt\Extension\Thirdwave\Export;
 
+use Bolt\Content;
+use Bolt\Storage;
+use Doctrine\DBAL\Connection;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 
 /**
@@ -16,6 +22,21 @@ class Controller implements ControllerProviderInterface
 
 
     /**
+     * @var Extension
+     */
+    protected $extension;
+
+
+    /**
+     * @param Extension $extension
+     */
+    public function __construct(Extension $extension)
+    {
+        $this->extension = $extension;
+    }
+
+
+    /**
      * @param Application $app
      * @return ControllerCollection
      */
@@ -23,10 +44,91 @@ class Controller implements ControllerProviderInterface
     {
         $routes = $app['controllers_factory'];
 
-        $routes->match('/export', array($this, 'export'))->bind('export');
-        $routes->match('/export/count', array($this, 'count'))->bind('export_count');
-        $routes->match('/export/download', array($this, 'download'))->bind('export_download');
+        $routes->get('/export', array($this, 'export'))->bind('export');
+        $routes->post('/export/download', array($this, 'download'))->bind('export_download');
+
+        // Check for permissions before executing the controller functions.
+        $routes->before(array($this->extension, 'checkPermissions'));
 
         return $routes;
+    }
+
+
+    /**
+     * @param Application $app
+     * @param Request     $request
+     * @return string
+     */
+    public function export(Application $app, Request $request)
+    {
+        $contenttype = null;
+        if ($request->query->has('contenttype')) {
+            $contenttype = $app['config']->get('contenttypes/' . $request->query->get('contenttype'));
+
+            $this->expandRelations($contenttype, $app);
+        }
+
+        return $app['render']->render('@export/index.twig', array(
+          'contenttypes' => $app['config']->get('contenttypes'),
+          'contenttype'  => $contenttype,
+          'base_columns' => array_flip(Content::getBaseColumns())
+        ));
+    }
+
+
+    /**
+     * @param Application $app
+     * @param Request     $request
+     * @return Response
+     */
+    public function download(Application $app, Request $request)
+    {
+        $profile  = $request->request->get('export');
+        $filename = !empty($profile['file']) ? $profile['file'] . '.csv' : date('Ymd') . '-' . $profile['contenttype'] . '.csv';
+        $query    = new ExportQuery($app['db'], $app['storage']);
+        $rows     = $query->profile($profile)->results();
+
+        if (isset($profile['header'])) {
+            array_unshift($rows, $profile['fields']);
+        }
+
+        $csv = '';
+
+        foreach ($rows as $row) {
+            $csv .= '"' . implode('"' . "\t" . '"', $row) . '"' . "\n";
+        }
+
+        $csv = mb_convert_encoding($csv, 'UTF-16LE', 'UTF-8');
+
+        return new Response(chr(255) . chr(254) . $csv, 200, array(
+          'Content-Description'       => 'File Transfer',
+          'Content-Type'              => 'application/vnd.ms-exce',
+          'Content-Disposition'       => 'attachment; filename=' . $filename,
+          'Content-Transfer-Encoding' => 'binary',
+          'Expires'                   => 0,
+          'Cache-Control'             => 'must-revalidate, post-check=0, pre-check=0',
+          'Pragma'                    => 'public',
+          'Content-Length'            => strlen($csv)
+        ));
+    }
+
+
+    /**
+     * @param array       $contenttype
+     * @param Application $app
+     */
+    protected function expandRelations(array &$contenttype, Application $app) {
+        if ( empty($contenttype['relations']) ) {
+            return;
+        }
+
+        foreach ( $contenttype['relations'] as $relation => &$properties ) {
+            $items = $app['storage']->getContent($relation);
+
+            $properties['values'] = [];
+            foreach ( $items as $item ) {
+                $properties['values'][$item->values['id']] = $item->getTitle();
+            }
+        }
     }
 }
---
+++ src/ExportQuery.php
@@ -0,0 +1,198 @@
+<?php namespace Bolt\Extension\Thirdwave\Export;
+
+use Bolt\Storage;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Query\QueryBuilder;
+
+
+/**
+ * Class ExportQuery
+ *
+ * @author  G.P. Gautier <ggautier@thirdwave.nl>
+ * @version 0.5.0, 2016/06/09
+ */
+class ExportQuery
+{
+
+
+    /**
+     * @var Connection
+     */
+    protected $connection;
+
+
+    /**
+     * @var Storage
+     */
+    protected $storage;
+
+
+    /**
+     * @var QueryBuilder
+     */
+    protected $query;
+
+
+    /**
+     * @var array
+     */
+    protected $parameters = array();
+
+
+    /**
+     * @var string
+     */
+    protected $contenttype;
+
+
+    /**
+     * @param Connection $connection
+     * @param Storage    $storage
+     */
+    public function __construct(Connection $connection, Storage $storage)
+    {
+        $this->connection = $connection;
+        $this->storage    = $storage;
+        $this->query      = $connection->createQueryBuilder();
+    }
+
+
+    /**
+     * @return array
+     */
+    public function results()
+    {
+        return $this->connection->executeQuery($this->query, $this->parameters)->fetchAll();
+    }
+
+
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        $this->query->select('COUNT(id) AS rows');
+
+        $count = $this->connection->executeQuery($this->query, $this->parameters)->fetch();
+
+        return $count['rows'];
+    }
+
+
+    /**
+     * @param array $profile
+     * @return $this
+     */
+    public function profile(array $profile)
+    {
+        return $this
+          ->contenttype($profile['contenttype'])
+          ->fields($profile['fields'])
+          ->filters($profile['filters'] ?: array())
+          ->relations($profile['relations'] ?: array())
+          ->sorting($profile['sorting'] ?: array());
+    }
+
+
+    /**
+     * @param string $contenttype
+     * @return $this
+     */
+    public function contenttype($contenttype)
+    {
+        $this->contenttype = $contenttype;
+
+        $this->query->from($this->storage->getTablename($contenttype), $contenttype);
+
+        return $this;
+    }
+
+
+    /**
+     * @param array $fields
+     * @return $this
+     */
+    public function fields(array $fields)
+    {
+        $this->query->select($fields);
+
+        return $this;
+    }
+
+
+    /**
+     * @param array $filters
+     * @return $this
+     */
+    public function filters(array $filters)
+    {
+        if (empty($filters)) {
+            return $this;
+        }
+
+        $where = $this->query->expr()->andX();
+
+        for ($i = 0; $i < count($filters['fields']); $i++) {
+            $operation = $filters['operators'][$i];
+
+            switch ($operation) {
+                case 'isNull':
+                case 'isNotNull':
+                    $where->add($this->query->expr()->$operation($filters['fields'][$i]));
+                    break;
+                default:
+                    $where->add($this->query->expr()->$operation($filters['fields'][$i], '?'));
+
+                    $this->parameters[] = $filters['values'][$i];
+
+                    break;
+            }
+        }
+
+        $this->query->where($where);
+
+        return $this;
+    }
+
+
+    /**
+     * @param array $relations
+     * @return $this
+     */
+    public function relations(array $relations) {
+        foreach ( $relations as $relation => $keys ) {
+            $where = $this->query->expr()->andX();
+            $where->add($this->query->expr()->eq('relations.from_contenttype', $this->query->expr()->literal($this->contenttype)));
+            $where->add($this->query->expr()->eq('relations.from_id', $this->contenttype . ".id"));
+            $where->add($this->query->expr()->eq('relations.to_contenttype', $this->query->expr()->literal($relation)));
+            $where->add($this->query->expr()->in('relations.to_id', $keys));
+
+            $this->query->innerJoin(
+              $this->contenttype,
+              $this->storage->getTablename('relations'),
+              'relations',
+              $where
+            );
+        }
+
+        return $this;
+    }
+
+
+    /**
+     * @param array $sorting
+     * @return $this
+     */
+    public function sorting(array $sorting)
+    {
+        for ($i = 0; $i < count($sorting['fields']); $i++) {
+            $this->query->addOrderBy(
+              $sorting['fields'][$i],
+              strtoupper($sorting['directions'][$i])
+            );
+        }
+
+        return $this;
+    }
+}
